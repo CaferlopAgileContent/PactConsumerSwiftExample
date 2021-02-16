7582a6ab022c3a94b1cf4483f4b869b0f165c267
Carlos Fernandez <carlos.fernandez@agilecontent.com>
2021-02-17 00:02:51 +0100

    Modified run pact tests script

--- PactConsumerSwiftExample/RunPactTests.sh
+++ PactConsumerSwiftExample/RunPactTests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xcodebuild clean build test -workspace ../PactConsumerSwiftExample.xcworkspace -scheme "PactConsumerSwiftExampleForTests" -destination "platform=iOS Simulator,name=iPhone 12,OS=14.3"
+xcodebuild clean build test -workspace ../PactConsumerSwiftExample.xcworkspace -scheme "PactConsumerSwiftExampleForTests" -destination "platform=iOS Simulator,name=iPhone 12"
   
